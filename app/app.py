9bfe71d41c72a3e547b4b08c216973a6bb3051bf
El Ouarary Abdelaziz <122648837+ElOuarary@users.noreply.github.com>
2025-10-24 23:54:15 +0100

    Moved to a new version of the app where the configuration of the summarizer llm is setted

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 import io
-import pymupdf
+from src.llm.summarizer import Summarizer
 import streamlit as st
 from transformers import pipeline
 
@@ -22,13 +22,6 @@ with st.sidebar:
     st.text_input('Configure your Notion Database ID')
     st.selectbox('Model', ['bert', 'google'])
 
-def extract_file_text(file_bytes):
-    pages = []
-    with pymupdf.open(io.BytesIO(file_bytes)) as f:
-        for page in f.pages:
-            pages.append(page)
-            print(page)
-
 columns = st.columns(
     2,
     gap='large'
@@ -36,33 +29,28 @@ columns = st.columns(
 
 with columns[0]:
     st.header('Upload File')
-    uploaded_files = st.file_uploader(
+    uploaded_file = st.file_uploader(
         label='Upload files',
-        type=['pdf', 'txt', 'docs', 'doc'],
-        accept_multiple_files=True,
+        type=['pdf', 'txt', 'docs', 'doc']
     )
-    if uploaded_files:
-        pass
-        st.write(f'Uploaded {len(uploaded_files)} {'file' if len(uploaded_files) == 1 else 'files'}')
-        for file in uploaded_files:
-            st.write(f'{file.name} ({file.size} bytes)')
+    if uploaded_file:
+        st.write(f'{uploaded_file.name} ({uploaded_file.size} bytes)')
 
 with columns[1]:
     st.header('Processing Options')
     auto_process = st.checkbox('Auto-process on upload', value=True)
     show_summary = st.checkbox('Show summary preview', value=True)
-    if st.button("Process All Files", disabled=not uploaded_files):
+    if st.button("Process All Files", disabled=not uploaded_file):
         pass
     
 
 st.title('Result')    
-if uploaded_files:
-    summarizer = pipeline('summarization', model='facebook/bart-large-cnn')
-    for file in uploaded_files:
-        st.title('Content')
-        content = io.StringIO(file.getvalue().decode('utf-8'))
-        st.write(content)
-        st.title('Summary')
+if uploaded_file:
+    summarizer = Summarizer()
+    st.title('Content')
+    content = io.StringIO(uploaded_file.getvalue().decode('utf-8'))
+    st.write(content)
+    st.title('Summary')
         
-        summary = summarizer(content.getvalue())[0]['summary_text']
-        st.write(summary)
+    summary = summarizer(content.getvalue())[0]['summary_text']
+    st.write(summary)
--- src/summarizer.py
+++ src/summarizer.py
@@ -1,9 +1,41 @@
+import logging
 from transformers import pipeline
+import yaml
 
 class Summarizer:
-    def __init__(self, model=None):
-        self.model = model
-        self.summarizer = pipeline('summarization',  model='facebook/bart-large-cnn')
-        
-    def summarize(self, text):
-        return self.summarizer(text)
+    def __init__(self, model):
+        self.logger = logging.getLogger(__name__)
+        try:
+            with open('config/model_config.yaml', 'r') as f:
+                config = yaml.safe_load(f)
+        except:
+            self.logger.error('Could not laod the configuration file of the model')
+            self.model = model
+        self.model_name = config[model].name
+        self.context_window = config[model].context_window
+        self.max_tokens = config[model].max_tokens
+        self.summarization_min_length = config[model].summarization.min_length
+        self.summarization_max_length = config[model].summarization.max_length
+        try:
+            self.summarizer = pipeline("summarization", model=self.model_name, device=-1)
+        except Exception as e:
+            self.logger.warning(f'Failed to load the model {model}')
+            self.summarizer = None
+
+
+def summarize(self, text):
+    if not self.summarizer:
+        self.logger.warning(f'{self.model} was not loaded')
+        return None
+    try:
+        if len(text) <= 1024:
+            result = self.summarizer(
+                text    
+            )
+            return result
+    except Exception as e:
+        self.logger.error(f'Summarization failed: {e}')
+        return None
+    
+if __name__ == '__main__':
+    pass
